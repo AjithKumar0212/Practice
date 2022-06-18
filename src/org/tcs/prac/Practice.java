b1e6060496b31a16cb504cb006e463d83bab3938
Ajithkumar0212 <Ajithniran07@gmail.com>
2022-06-18 23:21:14 +0530

    new

---
+++ src/org/tcs/prac/Practice.java
@@ -0,0 +1,46 @@
+package org.tcs.prac;
+
+import java.util.Scanner;
+
+public class Practice {
+public static void main(String[] args) 
+	
+{
+	// TODO Auto-generated method stub
+Scanner e =new Scanner(System.in);
+
+System.out.println("phone id ?");
+int phoneId=e.nextInt();
+System.out.println("phone id is "+phoneId);
+System.out.println("phone name is ");
+String phoneName=e.next();
+System.out.println("phone Name is "+phoneName);
+System.out.println("serialnumber ? ");
+short serialNumber =e.nextShort();
+System.out.println("serial Number is "+serialNumber);
+System.out.println("camera pixel in mb");
+byte cameraPixel=e.nextByte();
+System.out.println("camera pixel in mb is "+cameraPixel);
+System.out.println("batteryCapacity");
+int batteryCapacity = e.nextInt();
+System.out.println("battery capacity is "+batteryCapacity);
+System.out.println("ordernum");
+long orderno = e.nextLong();
+System.out.println("order no is"+orderno);
+System.out.println("price?");
+float price = e.nextFloat();
+System.out.println("price is "+price);
+System.out.println("x factor");
+double xFactor = e.nextDouble();
+System.out.println("X factor is "+xFactor);
+e.nextLine();
+System.out.println("userName ?");
+String userName = e.nextLine();
+System.out.println("user name is"+userName);
+System.out.println("phone is good?");
+boolean good = e.nextBoolean();
+System.out.println(" phone is in good "+good);
+
+
+}
+}
